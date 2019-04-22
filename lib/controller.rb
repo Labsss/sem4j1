3f9f5e5625dd65287bc634628c29b7c4de13b697
notmoebius <notmoebius@gmail.com>
2019-04-22 15:03:03 +0200

    update gossip, index, new_gossip, show

--- lib/controller.rb
+++ lib/controller.rb
@@ -17,4 +17,15 @@ class ApplicationController < Sinatra::Base
   get '/gossips/:id' do
     erb :show, locals: {id: params['id'].to_i, gossips: Gossip.find(params['id'].to_i)}
   end
+
+  get '/gossips/:id/edit' do
+    puts "coucou je suis dans edit"
+    erb :edit locals: {id: params['id'].to_i, gossips: Gossip.update(params['id'].to_i)}
+    #Gossip.update(params['id'].to_i)
+  end
+
+  get '/gossips/edit' do
+    puts "ja fais update"
+    #erb :show locals: {id: params['id'].to_i, gossips: Gossip.update(params['id'].to_i)}
+  end
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -2,39 +2,44 @@ require 'csv'
 require 'pry'
 
 class Gossip
-    attr_accessor :author, :content
+  attr_accessor :author, :content
 
-    def initialize(author, content)
-        @content = content
-        @author = author
+  def initialize(author, content)
+      @content = content
+      @author = author
+    end
+
+  def save
+      CSV.open("./db/gossip.csv", "a") do |csv|
+          csv << [@author, @content]
+          #csv << ["Mon super auteur", "Ma super description"]
       end
+  end
 
-    def save
-        CSV.open("./db/gossip.csv", "a") do |csv|
-            csv << [@author, @content]
-            #csv << ["Mon super auteur", "Ma super description"]
+  def self.all
+      all_gossips = []
+
+      CSV.foreach("./db/gossip.csv", quote_char: '"', row_sep: :auto, headers: true) do |row|
+        gossip_temp = Gossip.new(row[0],row[1])
+        all_gossips << gossip_temp
+      end
+      all_gossips
+  end
+
+  def self.find(id)
+  gossips = []
+  CSV.read("./db/gossip.csv").each_with_index do |row, index|
+    if (id == index+1)
+      gossips << Gossip.new(row[0], row[1])
+      break
         end
-    end
+      end
+      return gossips
+  end
 
-    def self.all
-        all_gossips = []
-
-        CSV.foreach("./db/gossip.csv", quote_char: '"', row_sep: :auto, headers: true) do |row|
-          gossip_temp = Gossip.new(row[0],row[1])
-          all_gossips << gossip_temp
-        end
-        all_gossips
-    end
-
-    def self.find(id)
-		gossips = []
-		CSV.read("./db/gossip.csv").each_with_index do |row, index|
-			if (id == index+1)
-  			gossips << Gossip.new(row[0], row[1])
-  			break
-  		    end
-        end
-        return gossips
-    end
-    #binding.pry
+  def self.update(id)
+    puts id
+    
+  end
+  #binding.pry
 end
